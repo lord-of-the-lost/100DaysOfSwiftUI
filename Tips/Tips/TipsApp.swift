fb5f8074163e19a3a7c88252e724c453a8fb801f
lord-of-the-lost <ignatov.99@yandex.ru>
2025-07-11 18:29:39 +0300

    start TipsApp

---
+++ Tips/Tips/TipsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipsApp.swift
+//  Tips
+//
+//  Created by Николай Игнатов on 11.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct TipsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
